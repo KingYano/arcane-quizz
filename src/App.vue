9ad34fc295def49485324d1fb574af293ae807a5
Yanis <belkadi.yanis.l@gmail.com>
2023-12-16 19:41:31 +0100

    Ajout des assets d'image, configuraiton du base.css et création du composant HeaderSection

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,18 @@
-<script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
+    <HeaderSection></HeaderSection>
+
 
   <main>
-    <TheWelcome />
+    
   </main>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-}
+<script setup lang="ts">
+import HeaderSection from './components/HeaderSection/HeaderSection.vue';
+  console.log('ees')
+</script>
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
+<style lang="scss">
 
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
 </style>
---
+++ src/components/HeaderSection/HeaderSection.vue
@@ -0,0 +1,26 @@
+<template>
+  <header>
+    <div class="header">
+        <div class="header__container">
+            <div class="header__option">
+                <i class="ri-menu-fill"></i>
+            </div>
+            <div class="container__logo">
+                <img src="../../assets/logo/logo_arcane.svg" alt="logo de la série Arcane">
+            </div>
+            <div class="container__redirection">
+                <p>Disponible maintenant</p>
+                <img src="../../assets/logo/logo_netflix.svg" alt="logo Netflix">
+            </div>
+        </div>
+    </div>
+  </header>
+</template>
+
+<script lang="ts">
+  console.log('ees')
+</script>
+
+<style lang="scss">
+
+</style>
