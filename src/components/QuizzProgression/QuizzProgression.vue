419b8325404f6b0c718c865f3b4569b32c9840bf
Yanis <belkadi.yanis.l@gmail.com>
2023-12-27 20:47:09 +0100

    WIP QuizzQuestion & QuizzPrograssion componenet

---
+++ src/components/QuizzProgression/QuizzProgression.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="progression-container">
+    <div class="progression">
+      <span class="bubble" v-for="bubble in bubbles" :key="bubble.id">{{ bubble.number }}</span>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue';
+
+interface Bubble {
+  id: number;
+  number: string;
+}
+
+const bubbles = ref<Bubble[]>([
+  { id: 1, number: '1' },
+  { id: 2, number: '2' },
+  { id: 3, number: '3' },
+  { id: 4, number: '4' },
+  { id: 5, number: '5' },
+  { id: 6, number: '6' },
+  { id: 7, number: '7' },
+  { id: 8, number: '8' },
+]);
+</script>
+
+<style lang="scss">
+    @import './QuizzProgression.scss';
+</style>
+
---
+++ src/components/QuizzQuestion/QuizzQuestion.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="quizz-container">
+    
+  </div>
+</template>
+
+<script setup lang="ts">
+  import QuizzProgression from '../QuizzProgression/QuizzProgression.vue';
+</script>
+
+<style lang="scss">
+    @import './QuizzQuestion.scss'
+</style>
