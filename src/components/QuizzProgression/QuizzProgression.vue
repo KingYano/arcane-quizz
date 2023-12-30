350fa11a550421bb1c9f02eafccfb42667411e6e
KingYano <belkadi.yanis.l@gmail.com>
2023-12-30 20:57:25 +0100

    Modification du composant QuizzProgression pour que les bulles changent de couleurs suivant les réponses

--- src/components/QuizzProgression/QuizzProgression.vue
+++ src/components/QuizzProgression/QuizzProgression.vue
@@ -1,18 +1,37 @@
 <template>
   <div class="progression-container">
     <div class="progression">
-      <span class="bubble" v-for="bubble in bubblesRef" :key="bubble.id">{{ bubble.number }}</span>
+      <span class="bubble"
+            v-for="(bubble, index) in bubblesRef"
+            :key="bubble.id"
+            :class="{
+              'bubble--correct': computedQuestionStatus[index] === 'correct',
+              'bubble--incorrect': computedQuestionStatus[index] === 'incorrect'
+            }">
+        {{ bubble.number }}
+      </span>
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue';
-import { bubbles } from '@/components/QuizzProgression/data/bubblesData';
+  import { ref, defineProps, computed } from 'vue';
+  import { bubbles } from '@/components/QuizzProgression/data/bubblesData';
 
-const bubblesRef = ref(bubbles);
+  const bubblesRef = ref(bubbles);
+
+  const props = defineProps({
+    questionStatus: {
+      type: Array,
+      default: () => []
+    }
+  });
+
+  const computedQuestionStatus = computed(() => {
+    return props.questionStatus ?? [];
+  });
 </script>
 
 <style lang="scss">
-@import './QuizzProgression.scss';
+  @import './QuizzProgression.scss';
 </style>
--- src/components/QuizzQuestion/QuizzQuestion.vue
+++ src/components/QuizzQuestion/QuizzQuestion.vue
@@ -2,7 +2,7 @@
   <main class="main-question" :style="backgroundStyle">
     <div class="quizz-container">
       <div class="quizz-progression">
-        <QuizzProgression></QuizzProgression>
+        <QuizzProgression :questionStatus="questionStatus"></QuizzProgression>
       </div>
       <div class="quizz-texte">
         <h3>{{ questions[currentQuestionIndex].question }}</h3>
@@ -22,54 +22,61 @@
   </main>
 </template>
 
-
 <script setup lang="ts">
-  import { ref, computed } from 'vue';
-  import QuizzProgression from '../QuizzProgression/QuizzProgression.vue';
-  import { questions } from '@/components/QuizzQuestion/data/questionsData';
+import {ref, computed} from 'vue';
+import QuizzProgression from '../QuizzProgression/QuizzProgression.vue';
+import { questions } from '@/components/QuizzQuestion/data/questionsData';
 
-  const currentQuestionIndex = ref<number>(0);
-  const selectedAnswer = ref<string>("");
-  const isAnswerSubmitted = ref<boolean>(false);
+const currentQuestionIndex = ref<number>(0);
+const selectedAnswer = ref<string>("");
+const isAnswerSubmitted = ref<boolean>(false);
+const questionStatus = ref<Array<'correct' | 'incorrect' | 'unanswered'>>(new Array(questions.length).fill('unanswered'));
 
-  const selectAnswer = (answer: string) => {
-    if (isAnswerSubmitted.value) {
-      return;
-    }
-    selectedAnswer.value = answer;
-    isAnswerSubmitted.value = true;
+const selectAnswer = async (answer: string) => {
+  if (isAnswerSubmitted.value) {
+    return;
+  }
+  selectedAnswer.value = answer;
+  isAnswerSubmitted.value = true;
+
+  if (answer === questions[currentQuestionIndex.value].correctAnswer) {
+    questionStatus.value[currentQuestionIndex.value] = 'correct';
+    console.log('correct');
+  } else {
+    questionStatus.value[currentQuestionIndex.value] = 'incorrect';
+    console.log('incorrect');
+  }
+};
+
+const goToNextQuestion = () => {
+  if (currentQuestionIndex.value < questions.length - 1) {
+    currentQuestionIndex.value++;
+    selectedAnswer.value = "";
+    isAnswerSubmitted.value = false;
+  } else {
+    console.log('fin du quizz !');
+  }
+};
+
+const addingClassAnswer = (answer: string) => {
+  if (!isAnswerSubmitted.value) return '';
+
+  if (answer === questions[currentQuestionIndex.value].correctAnswer) {
+    return 'quizz-response--correct';
+  } else if (answer === selectedAnswer.value) {
+    return 'quizz-response--incorrect';
+  }
+  return '';
+};
+
+const backgroundStyle = computed(() => {
+  return {
+    background: `url(./../${questions[currentQuestionIndex.value].imagePath}) no-repeat`
   };
-
-
-  const goToNextQuestion = () => {
-    if (currentQuestionIndex.value < questions.length - 1) {
-      currentQuestionIndex.value++;
-      selectedAnswer.value = "";
-      isAnswerSubmitted.value = false;
-    } else {
-      console.log('fin du quizz !');
-    }
-  };
-
-  const addingClassAnswer = (answer: string) => {
-    if (!isAnswerSubmitted.value) return '';
-
-    if (answer === questions[currentQuestionIndex.value].correctAnswer) {
-      return 'quizz-response--correct';
-    } else if (answer === selectedAnswer.value) {
-      return 'quizz-response--incorrect';
-    }
-    return '';
-  };
-
-  const backgroundStyle = computed(() => {
-    return {
-      background: `url(./../${questions[currentQuestionIndex.value].imagePath}) no-repeat`
-    };
-  });
+});
 
 </script>
 
 <style lang="scss">
-    @import './QuizzQuestion.scss';
+@import './QuizzQuestion.scss';
 </style>
