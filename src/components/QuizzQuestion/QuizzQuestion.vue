3b54edba3871fc56a8b683e33d19a6302c5855da
KingYano <belkadi.yanis.l@gmail.com>
2023-12-31 02:21:30 +0100

    Correction de style multiple fichier

--- src/components/QuizzQuestion/QuizzQuestion.vue
+++ src/components/QuizzQuestion/QuizzQuestion.vue
@@ -7,12 +7,14 @@
       <div class="quizz-texte">
         <h3>{{ questions[currentQuestionIndex].question }}</h3>
       </div>
-      <div class="quizz-responses">
-        <div class="quizz-response" v-for="(answer, index) in questions[currentQuestionIndex].answers"
-             :key="index"
-             :class="addingClassAnswer(answer)"
-             @click="selectAnswer(answer)">
-          {{ answer }}
+      <div class="quizz-container-responses">
+        <div class="quizz-choise-responses">
+          <button class="quizz-response" v-for="(answer, index) in questions[currentQuestionIndex].answers"
+                  :key="index"
+                  :class="addingClassAnswer(answer)"
+                  @click="selectAnswer(answer)">
+            {{ answer }}
+          </button>
         </div>
       </div>
       <div class="quizz-command">
@@ -23,60 +25,59 @@
 </template>
 
 <script setup lang="ts">
-import {ref, computed} from 'vue';
-import QuizzProgression from '../QuizzProgression/QuizzProgression.vue';
-import { questions } from '@/components/QuizzQuestion/data/questionsData';
+  import {ref, computed } from 'vue';
+  import QuizzProgression from '../QuizzProgression/QuizzProgression.vue';
+  import { questions } from '@/components/QuizzQuestion/data/questionsData';
 
-const currentQuestionIndex = ref<number>(0);
-const selectedAnswer = ref<string>("");
-const isAnswerSubmitted = ref<boolean>(false);
-const questionStatus = ref<Array<'correct' | 'incorrect' | 'unanswered'>>(new Array(questions.length).fill('unanswered'));
+  const currentQuestionIndex = ref<number>(0);
+  const selectedAnswer = ref<string>("");
+  const isAnswerSubmitted = ref<boolean>(false);
+  const questionStatus = ref<Array<'correct' | 'incorrect' | 'unanswered'>>(new Array(questions.length).fill('unanswered'));
 
-const selectAnswer = async (answer: string) => {
-  if (isAnswerSubmitted.value) {
-    return;
-  }
-  selectedAnswer.value = answer;
-  isAnswerSubmitted.value = true;
+  const selectAnswer = async (answer: string) => {
+    if (isAnswerSubmitted.value) {
+      return;
+    }
+    selectedAnswer.value = answer;
+    isAnswerSubmitted.value = true;
 
-  if (answer === questions[currentQuestionIndex.value].correctAnswer) {
-    questionStatus.value[currentQuestionIndex.value] = 'correct';
-    console.log('correct');
-  } else {
-    questionStatus.value[currentQuestionIndex.value] = 'incorrect';
-    console.log('incorrect');
-  }
-};
-
-const goToNextQuestion = () => {
-  if (currentQuestionIndex.value < questions.length - 1) {
-    currentQuestionIndex.value++;
-    selectedAnswer.value = "";
-    isAnswerSubmitted.value = false;
-  } else {
-    console.log('fin du quizz !');
-  }
-};
-
-const addingClassAnswer = (answer: string) => {
-  if (!isAnswerSubmitted.value) return '';
-
-  if (answer === questions[currentQuestionIndex.value].correctAnswer) {
-    return 'quizz-response--correct';
-  } else if (answer === selectedAnswer.value) {
-    return 'quizz-response--incorrect';
-  }
-  return '';
-};
-
-const backgroundStyle = computed(() => {
-  return {
-    background: `url(./../${questions[currentQuestionIndex.value].imagePath}) no-repeat`
+    if (answer === questions[currentQuestionIndex.value].correctAnswer) {
+      questionStatus.value[currentQuestionIndex.value] = 'correct';
+      console.log('correct');
+    } else {
+      questionStatus.value[currentQuestionIndex.value] = 'incorrect';
+      console.log('incorrect');
+    }
   };
-});
 
+  const goToNextQuestion = () => {
+    if (currentQuestionIndex.value < questions.length - 1) {
+      currentQuestionIndex.value++;
+      selectedAnswer.value = "";
+      isAnswerSubmitted.value = false;
+    } else {
+      console.log('fin du quizz !');
+    }
+  };
+
+  const addingClassAnswer = (answer: string) => {
+    if (!isAnswerSubmitted.value) return '';
+
+    if (answer === questions[currentQuestionIndex.value].correctAnswer) {
+      return 'quizz-response--correct';
+    } else if (answer === selectedAnswer.value) {
+      return 'quizz-response--incorrect';
+    }
+    return '';
+  };
+
+  const backgroundStyle = computed(() => {
+    return {
+      background: `url(./../${questions[currentQuestionIndex.value].imagePath}) no-repeat`
+    };
+  });
 </script>
 
 <style lang="scss">
-@import './QuizzQuestion.scss';
+  @import './QuizzQuestion.scss';
 </style>
